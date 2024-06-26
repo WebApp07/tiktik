0046396f797cd3ffe83e3f187413256c4c4f0fc4
WebApp07 <aminezouguig@proton.me>
2024-06-26 13:26:57 +0100

    Setup Project, Navbar

---
+++ sanity/schemas/schema.js
@@ -0,0 +1,8 @@
+import post from "./post";
+import user from "./user";
+import comment from "./comment";
+import postBy from "./postBy";
+
+export const schema = {
+  types: [post, user, postBy, comment],
+};
