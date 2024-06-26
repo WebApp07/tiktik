6f487487357a6fb335888e869cd816be5b86b677
WebApp07 <aminezouguig@proton.me>
2024-06-26 15:23:39 +0100

    Discover

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { NextPage } from "next";
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+const Footer: NextPage = () => (
+  <div className="mt-6 hidden xl:block">
+    <p className="text-gray-400 text-sm mt-5">
+      © Developed By Team AMINE 2024 TikTik
+    </p>
+  </div>
+);
 
 export default Footer;
