e675443666ee515ac3ebaec45e20e29646e6d319
WebApp07 <aminezouguig@proton.me>
2024-07-02 01:56:55 +0100

    dep

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
   reactStrictMode: true,
   images: {
     domains: ["encrypted-tbn1.gstatic.com", "lh3.googleusercontent.com"],
