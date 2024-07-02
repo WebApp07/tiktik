4e556f618639cf70ea218fe245404063525eb3aa
WebApp07 <aminezouguig@proton.me>
2024-07-02 02:11:00 +0100

    deploy

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -49,7 +49,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
                 height={62}
                 className="rounded-full"
                 src={post.postedBy.image}
-                alt="profile photo"
+                alt="photo profile"
                 layout="responsive"
               />
             </>
